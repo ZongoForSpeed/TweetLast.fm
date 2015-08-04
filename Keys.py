f426b9aec49c339880da23b18ecfc00cbffeccc7
ZongoForSpeed <tanguy.lambert@gmail.com>
2015-08-04 23:24:52 +0200

    Create Keys.py

---
+++ Keys.py
@@ -0,0 +1,23 @@
+# LAST.FM KEYS
+
+# You have to have your own unique two values for API_KEY and API_SECRET
+# Obtain yours from http://www.last.fm/api/account for Last.fm
+LASTFM_API_KEY = ""
+LASTFM_API_SECRET = ""
+
+# In order to perform a write operation you need to authenticate yourself
+LASTFM_USERNAME = ""
+LASTFM_PASSWORD_HASH = "" # pylast.md5("password")
+
+# TWITTER KEYS
+
+# The consumer keys can be found on your application's Details
+# page located at https://dev.twitter.com/apps (under "OAuth settings")
+TWITTER_API_KEY = ""
+TWIITER_API_SECRET = ""
+
+# The access tokens can be found on your applications's Details
+# page located at https://dev.twitter.com/apps (located
+# under "Your access token")
+TWITTER_ACCESS_TOKEN = ""
+TWITTER_ACCESS_TOKEN_SECRET = ""
